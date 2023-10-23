daee1cef97c8789f4edc679b3d68b6c5a4fd3069
Lwazi Jiyane <67145874+Lwazi-J@users.noreply.github.com>
2023-10-23 08:03:57 +0200

    Update PassByValueExample.java

--- oop/11_params/Variations/PassByValueExample.java
+++ oop/11_params/Variations/PassByValueExample.java
@@ -1,6 +1,9 @@
-/*In Java, arguments are passed to methods using a mechanism called "pass by value." Understanding pass by value is important because it determines how changes to variables within a method affect the original values passed as arguments.
+/*In Java, arguments are passed to methods using a mechanism called "pass by value." 
+Understanding pass by value is important because it determines how changes to variables within a method affect the original values passed as arguments.
 
-In pass by value, a copy of the value of an argument is passed to the method. This means that modifications made to the parameter within the method do not affect the original argument outside the method. Java passes the value of the variable, not the variable itself.
+In pass by value, a copy of the value of an argument is passed to the method. 
+This means that modifications made to the parameter within the method do not affect the original argument outside the method. 
+Java passes the value of the variable, not the variable itself.
 
 Let's see some examples to illustrate pass by value in Java:
 
@@ -20,4 +23,8 @@ public class PassByValueExample {
 }
 
 
-/* In the above example, the modifyValue method takes an int parameter called value. Inside the method, we assign a new value of 10 to the value parameter. However, when we invoke the modifyValue method in the main method and pass the num variable as an argument, the original value of num remains unchanged. This demonstrates that modifications made to the parameter within the method do not affect the original argument.*/
+/* In the above example, the modifyValue method takes an int parameter called value. Inside the method, 
+we assign a new value of 10 to the value parameter. 
+However, when we invoke the modifyValue method in the main method and pass the num variable as an argument, 
+the original value of num remains unchanged. 
+This demonstrates that modifications made to the parameter within the method do not affect the original argument.*/
